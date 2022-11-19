32a3f74d340d7cf2421c41eefa9a9de52a66a695
Estevao Pereira <estevaopp05@gmail.com>
2022-11-19 20:30:47 -0300

    Do operations

---
+++ Calculator/Calculator/CalculatorOp.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class CalculatorOp
+    {
+        public CalculatorOp()
+        {
+
+        }
+        public static double? DoOperation(double x, double y, char option)
+        {
+            double? result = null;
+            switch (option)
+            {
+                case 'A':
+                    result = x + y;
+                    break;
+                case 'S':
+                    result = x - y;
+                    break;
+                case 'M':
+                    result = x * y;
+                    break;
+                case 'D':
+                    result = null;
+                    if (y != 0)
+                    {
+                        result = x / y;
+                    }
+                    break;
+                default:
+                    result = null;
+                    break;
+            }
+            return result;
+        }
+    }
+}
